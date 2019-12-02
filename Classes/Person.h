88029ad6cedfa5f65a98239ec5abd303db3326fc
Fearless <489368600@qq.com>
2019-12-02 10:50:16 +0800

    提交项目

---
+++ Classes/Person.h
@@ -0,0 +1,20 @@
+//
+//  Person.h
+//  DebugDemo
+//
+//  Created by XFB on 2019/12/2.
+//  Copyright © 2019 ZhouCheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Person : NSObject
+
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, assign) NSInteger age;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Classes/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  DebugDemo
+//
+//  Created by XFB on 2019/12/2.
+//  Copyright © 2019 ZhouCheng. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
